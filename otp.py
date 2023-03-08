6bb24a9379ca090b2d1d46f34d8e943aabe739ee
Lorenzoantonelli <56717474+Lorenzoantonelli@users.noreply.github.com>
2023-03-08 13:50:51 +0100

    enh: new data folder and new password and SERVICE_ID logic (#4)
    * Removed store_password and double check
    
    * folder is now ~/.otp_data, new SERVICE_ID logic
    
    * Fix: double_check expected
    
    * enh: save_new_otp check duplicates before input
    
    * enh: new password logic
    
    * fix: some functions needs password multiple times
    
    * enh: removed unused import

--- otp.py
+++ otp.py
@@ -1,68 +1,110 @@
 #! /usr/bin/env python3
 
 import keyring
-from os import listdir, remove, mkdir, path, environ
+from os import listdir, remove, makedirs, path, environ
 import subprocess
-from pathlib import Path
 import sys
 import json
 import argparse
 from getpass import getpass
 from datetime import datetime
 import pyotp
+from binascii import Error as binascii_error
+import random
+import string
+import hashlib
 
-SERVICE_ID = "PLACEHOLDER_SERVICE_ID"
-FOLDER_NAME = "OTP_DATA"
-EXECUTABLE_DIR = Path(__file__).parent.absolute()
-ABSOLUTE_FOLDER_PATH = path.join(EXECUTABLE_DIR, FOLDER_NAME)
-
-
-def set_pyinstaller_path():
-    if getattr(sys, 'frozen', False):
-        global EXECUTABLE_DIR
-        global ABSOLUTE_FOLDER_PATH
-        executable_path = sys.executable
-        EXECUTABLE_DIR = path.dirname(executable_path)
-        ABSOLUTE_FOLDER_PATH = path.join(EXECUTABLE_DIR, FOLDER_NAME)
+SERVICE_ID = None
+DATA_FOLDER = "~/.otp_data/OTP"
+MAIN_FOLDER = "~/.otp_data"
 
 
-def check_service_id():
-    if not getattr(sys, 'frozen', False):
-        if SERVICE_ID == "PLACEHOLDER_SERVICE_ID":
-            randomize_service_id()
+def init_service_id():
+
+    global SERVICE_ID
+    service_id_json_path = path.join(MAIN_FOLDER, "service_id.json")
+
+    if not path.isfile(service_id_json_path):
+        SERVICE_ID = generate_service_id()
+
+        with open(service_id_json_path, "w") as f:
+            json.dump(SERVICE_ID, f)
+
+    else:
+        with open(service_id_json_path, "r") as f:
+            SERVICE_ID = json.load(f)
 
 
-def randomize_service_id():
-    import random
-    import string
-    service_id = ''.join(random.choice(
-        string.ascii_letters + string.digits) for _ in range(16))
-    subprocess.run(
-        ['sed', '-i', "", '-e', 's/^SERVICE_ID = "PLACEHOLDER_SERVICE_ID"/SERVICE_ID = "{}"/g'.format(service_id), __file__])
+def generate_service_id():
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16))
 
 
-def init_folder():
-    if not path.isdir(ABSOLUTE_FOLDER_PATH):
-        mkdir(ABSOLUTE_FOLDER_PATH)
+def init_folders():
+    global DATA_FOLDER
+    global MAIN_FOLDER
+    DATA_FOLDER = path.expanduser(DATA_FOLDER)
+    MAIN_FOLDER = path.expanduser(MAIN_FOLDER)
+    if not path.isdir(DATA_FOLDER):
+        makedirs(DATA_FOLDER)
 
 
-def get_password(double_check=False, store_password=False):
+# this will ensure that the password is the same for all the services, even when is not stored in the keychain
+def init_password():
+    password_hash_path = path.join(MAIN_FOLDER, "password_hash.json")
+
+    if not path.isfile(password_hash_path):
+        while True:
+            password = getpass("Insert the password: ")
+            password_confirm = getpass("Confirm the password: ")
+            if password == password_confirm:
+                break
+
+        store_password = input(
+            "Do you want to store the password in the keychain? [y/N] ")
+        if store_password.lower() == "y":
+            store_password_to_keychain(password)
+
+        salt = "otp_salt_for_passwords"
+        hashed_password = hashlib.sha256(
+            (password + salt).encode()).hexdigest()
+
+        with open(password_hash_path, "w") as f:
+            json.dump(hashed_password, f)
+
+
+def get_password():
     password = keyring.get_password(SERVICE_ID, "password")
     if not password:
-        if double_check:
-            while True:
-                password = getpass("Insert the password: ")
-                password_confirm = getpass("Confirm the password: ")
-                if password != password_confirm:
-                    print("Passwords do not match, please try again",
-                          file=sys.stderr)
-                else:
-                    break
-        else:
+        attempts = 0
+        while True:
             password = getpass("Insert the password: ")
-        if store_password:
-            keyring.set_password(SERVICE_ID, "password", password)
+            if validate_password(password):
+                break
+            else:
+                attempts += 1
+
+                if attempts == 3:
+                    print("Too many attempts, exiting", file=sys.stderr)
+                    exit(1)
+
+                print("Wrong password, please try again", file=sys.stderr)
+
     return password
+
+
+def validate_password(password):
+    password_hash_path = path.join(MAIN_FOLDER, "password_hash.json")
+
+    with open(password_hash_path, "r") as f:
+        password_hash = json.load(f)
+
+    salt = "otp_salt_for_passwords"
+
+    return password_hash == hashlib.sha256((password + salt).encode()).hexdigest()
+
+
+def store_password_to_keychain(password):
+    keyring.set_password(SERVICE_ID, "password", password)
 
 
 def encrypt_string(string, password):
@@ -81,19 +123,27 @@ def decrypt_string(string, password):
     return result.decode('utf-8')
 
 
-def save_new_otp(service_name, otp_digit=6, otp_period=30, store_password=False):
-    password = get_password(double_check=True, store_password=store_password)
+def check_service_exists(service_name):
+    return path.isfile(get_service_path(service_name))
 
-    otp_secret = input("OTP secret: ")
 
-    if path.isfile(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json")):
+def get_service_path(service_name):
+    return path.join(DATA_FOLDER, service_name + ".json")
+
+
+def save_new_otp(service_name, otp_digit=6, otp_period=30):
+    password = get_password()
+
+    if check_service_exists(service_name):
         print(f"{service_name} already exists")
         exit(1)
 
+    otp_secret = input("OTP secret: ")
+
+    # this will check is string is base32 encoded and correctly padded
     try:
-        import binascii
         pyotp.TOTP(otp_secret).now()
-    except binascii.Error:
+    except binascii_error:
         print("ERROR: Secret is not base32 encoded", file=sys.stderr)
         exit(1)
 
@@ -103,20 +153,20 @@ def save_new_otp(service_name, otp_digit=6, otp_period=30, store_password=False)
     data['otp_digit'] = otp_digit
     data['otp_period'] = otp_period
 
-    with open(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json"), "w") as f:
+    with open(get_service_path(service_name), "w") as f:
         json.dump(data, f)
 
     print(f"Item {service_name} saved successfully")
 
 
-def generate_otp(service_name, copy_to_clipboard=False, store_password=False):
-    password = get_password(store_password=store_password)
+def generate_otp(service_name, copy_to_clipboard=False, password_preloaded=None):
+    password = get_password() if password_preloaded is None else password_preloaded
 
-    if not path.isfile(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json")):
+    if not check_service_exists(service_name):
         print(f"ERROR: {service_name} does not exist", file=sys.stderr)
         exit(1)
 
-    with open(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json"), "r") as f:
+    with open(get_service_path(service_name), "r") as f:
         data = json.load(f)
 
     otp_secret = decrypt_string(data['otp_secret'], password)
@@ -126,8 +176,10 @@ def generate_otp(service_name, copy_to_clipboard=False, store_password=False):
 
     totp = pyotp.TOTP(otp_secret, digits=otp_digit, interval=otp_period)
     result = totp.now()
+
     if copy_to_clipboard:
         text_to_clipboard(result.encode('utf-8'))
+
     return result
 
 
@@ -141,7 +193,7 @@ def text_to_clipboard(text):
                 exit(0)
         elif 'DISPLAY' in environ:
             try:
-                p = subprocess.Popen(['xsel','-bi'], stdin=subprocess.PIPE)
+                p = subprocess.Popen(['xsel', '-bi'], stdin=subprocess.PIPE)
                 p.communicate(input=text)
             except FileNotFoundError:
                 print("xsel not found, is it installed?", file=sys.stderr)
@@ -151,19 +203,19 @@ def text_to_clipboard(text):
 
 
 def delete_otp(service_name):
-    if not path.isfile(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json")):
+    if not check_service_exists(service_name):
         print(f"ERROR: {service_name} does not exist", file=sys.stderr)
         exit(1)
 
     choice = input(f"Are you sure you want to delete {service_name}? [y/N] ")
     if choice.lower() == "y":
-        remove(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json"))
+        remove(get_service_path(service_name))
         print(f"Item {service_name} deleted successfully")
 
 
 def list_otp():
-    if path.isdir(ABSOLUTE_FOLDER_PATH):
-        files = sorted(listdir(ABSOLUTE_FOLDER_PATH))
+    if path.isdir(DATA_FOLDER):
+        files = sorted(listdir(DATA_FOLDER))
         for f in files:
             if f.endswith(".json"):
                 print(f[:-5])
@@ -171,17 +223,20 @@ def list_otp():
             print("No OTP found", file=sys.stderr)
 
 
-def export_all_otp(file_name, store_password=False):
-    password = get_password(store_password=store_password)
-    if path.isdir(ABSOLUTE_FOLDER_PATH):
-        files = sorted(listdir(ABSOLUTE_FOLDER_PATH))
+def export_all_otp(file_name):
+    password = get_password()
+
+    if path.isdir(DATA_FOLDER):
+        files = sorted(listdir(DATA_FOLDER))
         data = dict()
+
         file_name = file_name[:-5] if file_name.endswith(
             ".json") else file_name
         file_name += "_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".json"
+
         for otp_file in files:
             if otp_file.endswith(".json"):
-                with open(path.join(ABSOLUTE_FOLDER_PATH, otp_file), "r") as f:
+                with open(path.join(DATA_FOLDER, otp_file), "r") as f:
                     temp_data = json.load(f)
                     temp_data['otp_secret'] = decrypt_string(
                         temp_data['otp_secret'], password)
@@ -193,15 +248,15 @@ def export_all_otp(file_name, store_password=False):
 
 
 def export_all_encrypted_otp(file_name):
-    if path.isdir(ABSOLUTE_FOLDER_PATH):
-        files = sorted(listdir(ABSOLUTE_FOLDER_PATH))
+    if path.isdir(DATA_FOLDER):
+        files = sorted(listdir(DATA_FOLDER))
         data = dict()
         file_name = file_name[:-5] if file_name.endswith(
             ".json") else file_name
         file_name += "_encrypted_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".json"
         for otp_file in files:
             if otp_file.endswith(".json"):
-                with open(path.join(ABSOLUTE_FOLDER_PATH, otp_file), "r") as f:
+                with open(path.join(DATA_FOLDER, otp_file), "r") as f:
                     temp_data = json.load(f)
                     data[temp_data['service_name']] = temp_data
 
@@ -215,13 +270,15 @@ def import_all_otp(file_name):
         print(f"ERROR: {file_name} does not exist", file=sys.stderr)
         exit(1)
 
+    password = get_password()
+
     with open(file_name, "r") as f:
         data = json.load(f)
 
     for service_name, otp_data in data.items():
         otp_data['otp_secret'] = encrypt_string(
-            otp_data['otp_secret'], get_password())
-        with open(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json"), "w") as f:
+            otp_data['otp_secret'], password)
+        with open(get_service_path(service_name), "w") as f:
             json.dump(otp_data, f)
 
     print(f"Imported {len(data)} items")
@@ -236,37 +293,44 @@ def import_all_encrypted_otp(file_name):
         data = json.load(f)
 
     for service_name, otp_data in data.items():
-        with open(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json"), "w") as f:
+        with open(get_service_path(service_name), "w") as f:
             json.dump(otp_data, f)
 
     print(f"Imported {len(data)} items")
 
 
 def print_all_otp():
-    if path.isdir(ABSOLUTE_FOLDER_PATH):
-        files = sorted(listdir(ABSOLUTE_FOLDER_PATH))
+    if path.isdir(DATA_FOLDER):
+
+        password = get_password()
+
+        files = sorted(listdir(DATA_FOLDER))
         for f in files:
             if f.endswith(".json"):
-                with open(path.join(ABSOLUTE_FOLDER_PATH, f), "r") as f:
+                with open(path.join(DATA_FOLDER, f), "r") as f:
                     data = json.load(f)
                 print(
-                    f"{data['service_name']}: {generate_otp(data['service_name'])}")
+                    f"{data['service_name']}: {generate_otp(data['service_name'], password_preloaded=password)}")
 
 
 def delete_password():
     choice = input(
         "Are you sure you want to delete the password from keyring? [y/N] ")
     if choice.lower() == "y":
-        keyring.delete_password(SERVICE_ID, "password")
+        try:
+            keyring.delete_password(SERVICE_ID, "password")
+        except keyring.errors.PasswordDeleteError:
+            print("Password not found", file=sys.stderr)
+            exit(1)
         print("Password deleted successfully")
 
 
 def generate_qr_code(service_name):
-    if not path.isfile(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json")):
+    if not check_service_exists(service_name):
         print(f"ERROR: {service_name} does not exist", file=sys.stderr)
         exit(1)
 
-    with open(path.join(ABSOLUTE_FOLDER_PATH, service_name + ".json"), "r") as f:
+    with open(get_service_path(service_name), "r") as f:
         data = json.load(f)
 
     otp_secret = decrypt_string(data['otp_secret'], get_password())
@@ -284,8 +348,6 @@ def main():
     parser = argparse.ArgumentParser(description='OTP Manager')
     parser.add_argument('-a', '--add', metavar="service_name",
                         help='Add a new OTP', type=str)
-    parser.add_argument('-s', '--store', help='Store password',
-                        action='store_true')
     parser.add_argument('-g', '--generate',
                         metavar="service_name", help='Generate OTP', type=str)
     parser.add_argument('-d', '--delete', help='Delete OTP',
@@ -312,14 +374,14 @@ def main():
                         metavar="service_name", type=str)
     args = parser.parse_args()
 
-    set_pyinstaller_path()
-    check_service_id()
-    init_folder()
+    init_folders()
+    init_service_id()
+    init_password()
 
     if args.add:
-        save_new_otp(args.add, args.digits, args.duration, args.store)
+        save_new_otp(args.add, args.digits, args.duration)
     elif args.generate:
-        print(generate_otp(args.generate, args.copy, args.store))
+        print(generate_otp(args.generate, args.copy))
     elif args.delete:
         delete_otp(args.delete)
     elif args.list:
@@ -328,7 +390,7 @@ def main():
         if args.encrypted:
             export_all_encrypted_otp(args.export)
         else:
-            export_all_otp(args.export, args.store)
+            export_all_otp(args.export)
     elif args.import_otp:
         if args.encrypted:
             import_all_encrypted_otp(args.import_otp)
